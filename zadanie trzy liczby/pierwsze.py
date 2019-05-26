2c7b2ffda8df64eeab2fbffe60c8bbb347a92dcc
HP <agnieszkakulakgik@gmail.com>
2019-05-26 11:37:57 +0200

    agulator początek

---
+++ zadanie trzy liczby/pierwsze.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+1
+# ~/python/01_if.py
+
+op = "t"
+while op == "t":
+    a, b, c = input("Podaj trzy liczby oddzielone spacjami i przecinkami: ").split(" ")
+
+    print("Wprowadzono liczby:", a, b, c,)
+    print("\n Najmniejsza: ")
+
+    if a < b:
+        if a < c:
+            print(a)
+        else:
+            print(c)
+    elif b < c:
+        print(b)
+    else:
+        print(c)
+
+    op = input("Jeszcze raz (t/n)? ")
+
+print("By, by...")
